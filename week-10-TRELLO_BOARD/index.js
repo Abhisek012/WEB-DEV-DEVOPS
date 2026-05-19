fe951ed9a8284b909fb4b8f3616f5c694381f03d
ABHI <abhisekprusty012@gmail.com>
2026-05-19 22:54:06 +0530

    Building Trello-board

---
+++ week-10-TRELLO_BOARD/index.js
@@ -0,0 +1,44 @@
+//usersname , password | USERS table
+//organisation | ORGANISATION table
+//boards | BOARDS table
+//issues | ISSUES table
+
+const users =[{
+    id:1,
+    username: "Abhisek",
+    password: "123123"
+},{
+    id:2,
+    username:"Raman",
+    password:"456456"
+}]
+
+const organisation=[{
+    id:1,
+    title:"100xdevs",
+    description:"Learning coding platform",
+    admin:1,
+    member:[2]
+},{
+    id:2,
+    title:"ramans org",
+    description:"Experimenting",
+    admin:2,
+    member:[]
+}];
+
+const borads= [{
+    id:1,
+    title: "100xdevs website frontend",
+    organistion: 1
+}]
+
+const issues =[{
+    id:1,
+    title:"Add dark mode",
+    boradID: 1
+},{
+    id:2,
+    title:"Add 100xdevs dashboard",
+    boradID:2
+}]
