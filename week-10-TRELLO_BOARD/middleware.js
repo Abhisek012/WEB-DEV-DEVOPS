0f3fb3260e8371bfb6f02ca9eb90fdcd0700f694
ABHI <abhisekprusty012@gmail.com>
2026-05-21 23:18:06 +0530

    authmiddleware done!

--- week-10-TRELLO_BOARD/middleware.js
+++ week-10-TRELLO_BOARD/middleware.js
@@ -1,4 +1,24 @@
 function authmiddleware(){
-    
+    function authmiddleware(req,res, next){
+
+        const token = req.headers.token; //jwt
+
+        const decoded = jwt.verify(token,"rjdekjsdfjkljfkjej8473289urdkjdpassword");
+        const userId = decoded.userId;
+        if(userId){
+            req.userId;
+            next();
+
+
+        }else{
+            res.status(403).json({
+                message:"Token was incorrect"
+            })
+        }
+    }
+}
+
+module.exports = {
+    authmiddleware:authmiddleware
 }
 
