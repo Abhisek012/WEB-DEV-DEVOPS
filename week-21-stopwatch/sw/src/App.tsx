c5d21d0c80cd96410ecd11ef6f8c69ab386b6f1d
ABHI <abhisekprusty012@gmail.com>
2026-06-29 23:38:29 +0530

    changes

--- week-21-stopwatch/sw/src/App.tsx
+++ week-21-stopwatch/sw/src/App.tsx
@@ -1,7 +1,7 @@
 function App(){
   return <div style={{display:"flex" , justifyContent:"center" , width:"100vw", height: "100vh" , alignItems: "center"}}>
     Hi there
-    <div>
+    <div className="border">
       stopwatch
     </div>
   </div>
